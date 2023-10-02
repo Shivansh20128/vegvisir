251666aa2831110d7d25658e1c034aca70250f48
Shivansh <shivansh20128@iiitd.ac.in>
2023-10-02 22:42:29 +0530

    update

--- vegvisir/environments/__init__.py
+++ vegvisir/environments/__init__.py
@@ -1,8 +1,21 @@
-from vegvisir.environments import sensors, webserver
+# from vegvisir.environments import sensors, webserver
+#
+# default_environment = "webserver-basic"
+# available_environments = {
+#     "webserver-basic": webserver.WebserverBasic
+# }
+#
+# available_sensors = {
+#     "timeout": sensors.TimeoutSensor,
+#     "browser-file-watchdog": sensors.BrowserDownloadWatchdogSensor
+# }
+
+from vegvisir.environments import webserver, sensors, cl
 
 default_environment = "webserver-basic"
 available_environments = {
-    "webserver-basic": webserver.WebserverBasic
+    "webserver-basic": webserver.WebserverBasic,
+    "cross-layer": cl.CrossLayer
 }
 
 available_sensors = {
